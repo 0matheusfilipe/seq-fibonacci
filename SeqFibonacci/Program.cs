58552c1a2b60f2518db69aecbcc393cedace2230
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-08-06 23:36:40 -0300

    Add files via upload

---
+++ SeqFibonacci/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeqFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int termo1 = 0;
+            int termo2 = 1;
+            int somaTermos = 0;
+
+            Console.Write("Insira o número de valores da sequência: ");
+            int numValores = int.Parse(Console.ReadLine());
+            Console.WriteLine("Sequência de Fibonacci com " + numValores + " valores: ");
+            for (int i = 0; i < numValores; i++) 
+            {
+                if(i < numValores - 1)
+                {
+                    Console.Write(termo1 + ", ");
+                }
+                else
+                {
+                Console.Write(termo1);
+                }
+                somaTermos = termo1 + termo2;
+                termo1 = termo2;
+                termo2 = somaTermos;
+            }
+            Console.ReadKey();
+        }
+    }
+}
